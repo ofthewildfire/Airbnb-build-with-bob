4570e3ed3f24c004b033c97c28bd499da78ccefa
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-06-17 15:32:35 +0200

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,14 @@ function App() {
       <Navbar />
       <Hero />
       <div className="card-container">
-        <Card />
+        <Card
+          img="swimmer.png"
+          rating="5.0"
+          reviewCount={6}
+          country="USA"
+          title="Life Lessons with Katie Zaferes"
+          price={136}
+        />
       </div>
       <Footer />
     </>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,37 +1,29 @@
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
-export default function Card() {
+export default function Card({
+  img,
+  rating,
+  country,
+  reviewCount,
+  title,
+  price,
+}) {
+  // const imgValue = "./images/" + props.img;
   return (
     <div className="card">
       <span className="badge">Sold Out</span>
-      <img src="/images/swimmer.png" alt="Katie Zaferes" />
+      {/* <img src={imgValue} alt="Katie Zaferes" /> */}
+      <img src={"./images/" + img} alt="Katie Zaferes" />
       <div className="card--text">
         <div className="card--rating">
           <img src="/images/star.png" alt="star image for rating" />
-          <span>4.94</span>
-          <span>(6) - USA</span>
+          <span>{rating}</span>
+          <span>
+            ({reviewCount}) - {country}
+          </span>
         </div>
         <div className="card--title">
-          <p>Life lessons with Katie Zaferes</p>
+          <p>{title}</p>
           <p className="price">
-            From $136 <span>/ person</span>
+            From ${price} <span>/ person</span>
           </p>
         </div>
       </div>
